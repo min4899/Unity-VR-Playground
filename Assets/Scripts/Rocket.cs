a472b2cdffe707adf72eab8fa9e5fa41cc764fcb
Minwoo Soh <min4899@gmail.com>
2022-06-09 23:45:31 -0700

    Added explosion force for rocket when colliding.

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public float radius = 5f;
+    public float force = 700f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        Explode();
+        Destroy(gameObject);
+    }
+
+    void Explode() {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearbyObject in colliders) {
+            // Add force
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null) {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+    }
+}
